3052e71dca914b4be11654837f67f445ea8c0f17
Nitin Joshi <njoshi023@gmail.com>
2014-03-18 23:46:45 -0400

    Allow developers to specify which tests to run in Node via the command line through a comma separated values of regexes (prefixed with t= or tests=) for good flexibility. Will get this working in browser as well through a UI
    For example you can type in t=creat,reject OR tests='\d'

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var custom_tests = process.argv.slice(2);
+var args = process.argv.slice(2);
+
+/* 
+Look for tests|t = comma separated value of regexes
+For example you can type in t=creat,reject OR tests='\d'
+*/
+var custom_test_patterns = args.filter(function(elem) {	
+	return /^(tests|t)=/i.test(elem);
+})[0];
+
+if (custom_test_patterns) {	
+	// Extract out the comma separated patterns	
+	custom_test_patterns = custom_test_patterns.trim()
+																						 .substring(custom_test_patterns.indexOf('=') + 1)
+																						 .split(',');
+}
+
 require('./es6');
-require('./lib/main').run(custom_tests);
+require('./lib/main').run(custom_test_patterns);
