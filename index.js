8cc84c1b75d95aecbeb62b93ecffa0c79348f29f
Nitin Joshi <njoshi023@gmail.com>
2014-03-23 23:32:10 -0400

    Fix tab indentation and remove python style var names

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var args = process.argv.slice(2);
 Look for tests|t = comma separated value of regexes
 For example you can type in t=creat,reject OR tests='\d'
 */
-var custom_test_patterns = args.filter(function(elem) {	
+var customTestPatterns = args.filter(function(elem) {	
 	return /^(tests|t)=/i.test(elem);
 })[0];
 
-if (custom_test_patterns) {	
+if (customTestPatterns) {	
 	// Extract out the comma separated patterns	
-	custom_test_patterns = custom_test_patterns.trim()
-																						 .substring(custom_test_patterns.indexOf('=') + 1)
+	customTestPatterns = customTestPatterns.trim()
+																						 .substring(customTestPatterns.indexOf('=') + 1)
 																						 .split(',');
 }
 
 require('./es6');
-require('./lib/main').run(custom_test_patterns);
+require('./lib/main').run(customTestPatterns);
