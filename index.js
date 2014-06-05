3f24e6adcacb01e96cb2ec1043e69979c31935ad
Stefan Penner <stefan.penner@gmail.com>
2014-06-04 20:08:16 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var args = process.argv.slice(2);
 
-/* 
+/*
 Look for tests|t = comma separated value of regexes
 For example you can type in t=creat,reject OR tests='\d'
 */
-var customTestPatterns = args.filter(function(elem) {	
+var customTestPatterns = args.filter(function(elem) {
 	return /^(tests|t)=/i.test(elem);
 })[0];
 
-if (customTestPatterns) {	
-	// Extract out the comma separated patterns	
+if (customTestPatterns) {
+	// Extract out the comma separated patterns
 	customTestPatterns = customTestPatterns.trim()
-										 	.substring(customTestPatterns.indexOf('=') + 1)
-											.split(',');
+    .substring(customTestPatterns.indexOf('=') + 1)
+		.split(',');
 }
 
 require('./es6');
